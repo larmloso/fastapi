6b0f9bf4fb38bf0c6c7fa621acb06cb7e682870f
ramsonram <thanakorn.ka.61@ubu.ac.th>
2020-10-20 15:06:40 +0700

    add

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get("/")
 def home():
     return {"message": "hello World"}
+
+
+@app.get("/main")
+def home():
+    return {"abc": "hello World"}
